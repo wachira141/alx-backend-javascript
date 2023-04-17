cf47b88f3eada3be9d490eee9c84b32ca7fc1d6c
wachira140 <wachiraw424@gmail>
2023-04-17 17:06:38 +0300

    eslint warnings modify

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,12 @@
+#!/usr/bin/node
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+    //eslint-disable-next-line
     const task = true;
+    //eslint-disable-next-line
     const task2 = false;
   }
 
