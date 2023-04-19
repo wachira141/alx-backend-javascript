ef4353b8c5597c03899d5a3a26cd3b7febaceffc
wachira140 <wachiraw424@gmail>
2023-04-19 15:57:03 +0300

    rectify code exec errors

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => (
-      res.map((st) => ({
-        status: st.status,
-        value: st.status === 'fulfilled' ? st.value : String(st.reason),
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
       }))
     ));
 }
