b900b8c82955e660a9cca5183ea2be56a48c004b
wachira140 <wachiraw424@gmail>
2023-04-20 17:24:34 +0300

    js classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+export default class ClassRoom {
+  /**
+   * create a new @see Classroom.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+  */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,24 @@
+export default class Currency {
+  constructor(code, name) {
+    this._name = name;
+    this._code = code;
+  }
+get name() {
+  return this._name;
+}
+set name(name) {
+  this._name = name;
+}
+
+get code() {
+return this._code = code;
+}
+
+set code(code) {
+  this._code = code;
+}
+
+displayFullCurrency() {
+  return `${this._name} (${this._code})`
+}
+}
